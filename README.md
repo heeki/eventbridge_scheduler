a9c6093c13a56cb75c490dc97a32f1a211809c57
heeki <heekipark@gmail.com>
2023-10-24 15:15:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Overview
+This repository implements a sample Step Functions workflow that is triggered by an EventBridge Scheduler rule.
---
+++ src/fn1/fn.py
@@ -0,0 +1,13 @@
+import boto3
+import json
+from aws_xray_sdk.core import patch_all
+
+# initialization
+session = boto3.session.Session()
+client = session.client('sqs')
+patch_all()
+
+def handler(event, context):
+    output = event
+    print(json.dumps(output))
+    return output
